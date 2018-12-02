45f732879bb0f4ff611af20a67b51b03df32c955
alnsantosh <aln.santosh@gmail.com>
2018-12-02 17:59:55 +0530

    70. Climbing Stairs - O(n)
    Time Complexity - O(n)
    Space Complexity - O(n)

---
+++ ClimbingStairs.java
@@ -0,0 +1,57 @@
+/*
+70. Climbing Stairs
+
+https://leetcode.com/problems/climbing-stairs/description/
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+/*
+Idea - Used dp approach
+Hardcode dp[0] and dp[1]
+Number pf ways of reaching ith step is equal to number of ways of reaching (i-1)th step and (i-2)th step since we can go to ith step from (i-1)th and (i-2)th step.
+
+For more detailed explaination,
+https://leetcode.com/problems/climbing-stairs/solution/
+*/
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0)
+            return 0;
+        else if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        int[] dp=new int[n];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
+/*
+Time Complexity - O(n)
+Space Complexity - O(n)
+*/
