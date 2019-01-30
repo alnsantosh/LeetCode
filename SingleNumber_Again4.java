513754dd501370dd2c37790042d69227a22a96c7
alnsantosh <aln.santosh@gmail.com>
2019-01-30 13:58:39 +0530

    Create SingleNumber_Again4.java

---
+++ SingleNumber_Again4.java
@@ -0,0 +1,19 @@
+/*
+136. Single Number
+https://leetcode.com/problems/single-number/description/
+Idea - Use XOR operation
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            result=result^nums[i];
+        }
+        return result;
+    }
+}
+/*
+Time Complexity - O(n)
+Space Complexity - O(1)
+*/
